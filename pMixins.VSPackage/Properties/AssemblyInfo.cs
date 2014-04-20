292f58cbb44236fe22c567f32cd3e7315715f2a6
fdafdsa <faddafd@afdaf.com>
2014-04-20 14:23:41 +0200

    Fixed strong name key references

--- pMixins.VSPackage/Properties/AssemblyInfo.cs
+++ pMixins.VSPackage/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-
+[assembly: AssemblyKeyFile(@"..\..\..\pMixins.snk")]
 

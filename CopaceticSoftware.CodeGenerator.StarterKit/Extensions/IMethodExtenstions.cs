86d1ce1759e0c4f0ca657ba6e4fb3845896528b6
philip pittle <ppittlejunk@hotmail.com>
2014-04-30 17:48:31 +0200

    Added missing copyright notices

--- CopaceticSoftware.CodeGenerator.StarterKit/Extensions/IMethodExtenstions.cs
+++ CopaceticSoftware.CodeGenerator.StarterKit/Extensions/IMethodExtenstions.cs
@@ -1,4 +1,22 @@
-﻿using System.Collections.Generic;
+﻿//----------------------------------------------------------------------- 
+// <copyright file="IMethodExtenstions.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Wednesday, April 30, 2014 5:48:10 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ICSharpCode.NRefactory.TypeSystem;

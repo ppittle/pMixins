3fd275e4cdbd29e0ae11a21245f94b6c3a6e3d42
Philip Pittle <ppittlejunk@hotmail.com>
2014-05-03 14:29:56 +0200

    Added / Updated references to Ninject

--- pMixins.VisualStudio/pMixinsSolutionManager.cs
+++ pMixins.VisualStudio/pMixinsSolutionManager.cs
@@ -1,7 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//----------------------------------------------------------------------- 
+// <copyright file="pMixinsSolutionManager.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Saturday, May 3, 2014 2:27:45 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
 using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure;
 using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure.VisualStudioSolution;
 

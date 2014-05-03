b92066e2f7038f74a8f7f9b03a9f4ac06ebea000
Philip Pittle <ppittlejunk@hotmail.com>
2014-05-03 14:28:30 +0200

    Added custom Solution Manager

---
+++ pMixins.VisualStudio/pMixinsSolutionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure;
+using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure.VisualStudioSolution;
+
+namespace CopaceticSoftware.pMixins.VisualStudio
+{
+    public class pMixinsSolutionManager : SolutionManager
+    {
+        public pMixinsSolutionManager(Solution solution, IVisualStudioEventProxy visualStudioEventProxy) : 
+            base(solution, visualStudioEventProxy)
+        {
+        }
+    }
+}

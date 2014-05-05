b1f3cd112ba23beb9f5ff68196f723fca7313ef9
philip pittle <ppittlejunk@hotmail.com>
2014-05-05 22:29:58 +0200

    Added class for reading the contents of an open document window in Visual Studio

---
+++ CopaceticSoftware.CodeGenerator.StarterKit/Infrastructure/VisualStudioOpenDocumentReader.cs
@@ -0,0 +1,71 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="VisualStudioOpenDocumentReader.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Monday, May 5, 2014 3:58:03 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using EnvDTE;
+using log4net;
+
+namespace CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure
+{
+    public interface IVisualStudioOpenDocumentReader
+    {
+        string GetDocumentText();
+    }
+
+    /// <summary>
+    /// http://us.generation-nt.com/answer/read-entire-contents-envdte-document-help-21147682.html
+    /// </summary>
+    [DebuggerDisplay("{_classFileName}")]
+    public class VisualStudioOpenDocumentReader : IVisualStudioOpenDocumentReader
+    {
+        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        
+        private readonly TextDocument _textDocument;
+        private readonly string _classFileName;
+
+        public VisualStudioOpenDocumentReader(Document document)
+        {
+            //http://msdn.microsoft.com/en-us/library/ms228776.aspx
+            _textDocument = (EnvDTE.TextDocument)(document.Object("TextDocument"));
+            _classFileName = document.FullName;
+        }
+
+        public string GetDocumentText()
+        {
+            if (null == _textDocument)
+                return string.Empty;
+
+            try
+            {
+                var editPoint = _textDocument.StartPoint.CreateEditPoint();
+
+                return editPoint.GetText(_textDocument.EndPoint);
+            }
+            catch (Exception e)
+            {
+                _log.Error(
+                    string.Format("Exception reading TextDocument for file [{0}]: {1}",
+                        _classFileName, e.Message), e);
+
+                return string.Empty;
+            }
+        }
+    }
+}

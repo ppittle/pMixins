7acc3ee42326288420ffa05814c4515948c8414e
philip pittle <ppittlejunk@hotmail.com>
2014-05-05 22:47:46 +0200

    Added custom AssemblyResolver for Test to remove project reference to pMixins

---
+++ pMixins.CodeGenerator.Tests/IntegrationTests/Infrastructure/TestMicrosoftBuildProjectAssemblyReferenceResolver.cs
@@ -0,0 +1,42 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="TestMicrosoftBuildProjectAssemblyReferenceResolver.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Monday, May 5, 2014 10:47:11 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using CopaceticSoftware.pMixins.VisualStudio;
+using ICSharpCode.NRefactory.TypeSystem;
+using Microsoft.Build.Evaluation;
+
+namespace CopaceticSoftware.pMixins.CodeGenerator.Tests.IntegrationTests.Infrastructure
+{
+    public class TestMicrosoftBuildProjectAssemblyReferenceResolver :
+        pMixinsMicrosoftBuildProjectAssemblyReferenceResolver
+    {
+        private const string projectMatchingString =
+            "\\pMixins\\pMixins.CodeGenerator.Tests\\pMixins.CodeGenerator.Tests.csproj]";
+
+        protected override IEnumerable<ProjectReference> ResolveProjectReferences(Project project)
+        {
+            return
+                base.ResolveProjectReferences(project)
+                    //Don't include project reference to pMixins
+                    .Where(p => !p.ToString().EndsWith(projectMatchingString));
+
+        }
+    }
+}

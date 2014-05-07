37654046e16b236bdc8e85bd4dba9d9c94008c79
philip pittle <ppittlejunk@hotmail.com>
2014-05-07 21:41:16 +0200

    Added test stubs

---
+++ CopaceticSoftware.CodeGenerator.StarterKit.Tests/IntegrationTests/CanResolveTypeThatHasDependencyOnAnotherFileInTheSameProject.cs
@@ -0,0 +1,32 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="CanResolveTypeThatHasDependencyOnAnotherFileInTheSameProject.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Wednesday, May 7, 2014 7:50:10 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CopaceticSoftware.pMixins.Tests.Common;
+
+namespace CopaceticSoftware.CodeGenerator.StarterKit.Tests.IntegrationTests
+{
+    [TestNotWrittenYet]
+    public class CanResolveTypeThatHasDependencyOnAnotherFileInTheSameProject : IntegrationTestBase
+    {
+    }
+}

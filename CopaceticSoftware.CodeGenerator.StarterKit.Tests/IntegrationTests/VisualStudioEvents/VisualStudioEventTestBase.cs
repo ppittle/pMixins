af53b00b46001b1a5206725a1ae35e6b926563aa
philip pittle <ppittlejunk@hotmail.com>
2014-05-07 14:07:41 +0200

    Split up classes into individual files

---
+++ CopaceticSoftware.CodeGenerator.StarterKit.Tests/IntegrationTests/VisualStudioEvents/VisualStudioEventTestBase.cs
@@ -0,0 +1,137 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="VisualStudioEventTestBase.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Wednesday, May 7, 2014 2:07:06 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure.IO;
+using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure.VisualStudioSolution;
+using CopaceticSoftware.pMixins.Tests.Common;
+using Microsoft.Build.Evaluation;
+using Ninject;
+using Rhino.Mocks;
+
+namespace CopaceticSoftware.CodeGenerator.StarterKit.Tests.IntegrationTests.VisualStudioEvents
+{
+    public class VisualStudioEventTestBase : IntegrationTestBase
+    {
+        protected TestVisualStudioEventProxy EventProxy;
+        /// <summary>
+        /// <see cref="IKernel"/> specific to the test class.
+        /// It's ok to rebind this Kernel without side effects.
+        /// </summary>
+        protected IKernel TestSpecificKernel;
+
+        protected IFileWrapper MockFileWrapper;
+
+        protected IMicrosoftBuildProjectLoader MockMicrosoftBuildProjectLoader;
+
+        protected readonly Dictionary<string, string> MockFileWrapperBackingStore = 
+            new Dictionary<string, string>();
+
+        /// <summary>
+        /// Got tired fo forgetting to use TestSpecificKernel, need a reminder.
+        /// </summary>
+        protected static new IKernel Kernel
+        {
+            get { throw new Exception("Don't call Kernel.  Call TestSpecificKernel"); }
+        }
+
+        public override void MainSetup()
+        {
+            TestSpecificKernel = KernelFactory.BuildDefaultKernelForTests();
+
+            EventProxy = TestSpecificKernel.Get<TestVisualStudioEventProxy>();
+
+            MockFileWrapper = BuildMockFileReader();
+            TestSpecificKernel.Rebind<IFileWrapper>().ToMethod(c => MockFileWrapper);
+
+            MockMicrosoftBuildProjectLoader = BuildMockMicrosoftBuildProjectLoader();
+            TestSpecificKernel.Rebind<IMicrosoftBuildProjectLoader>().ToMethod(c => MockMicrosoftBuildProjectLoader);
+
+            //Set solution context
+            TestSpecificKernel.Get<ISolutionContext>().SolutionFileName =
+                MockSolutionFileContents.Solution.SolutionFileName;
+        }
+
+        protected virtual IFileWrapper BuildMockFileReader()
+        {
+            var fileWrapper = MockRepository.GenerateStub<IFileWrapper>();
+
+            //ReadAllText
+            fileWrapper
+                .Stub(x => x.ReadAllText(Arg<string>.Is.Anything))
+                .Do(
+                    (Func<string, string>)
+                        (filename =>
+                        {
+                            string result;
+                            if (MockFileWrapperBackingStore.TryGetValue(filename, out result))
+                                return result;
+
+                            throw new Exception(
+                                string.Format("Failed to read [{0}].  Key is not present in MockFileReaderBackingStore",
+                                    filename));
+                        }));
+                
+            //Exists
+            fileWrapper
+                .Stub(x => x.Exists(Arg<string>.Is.Anything))
+                .Do(
+                    (Func<string, bool>)
+                        (filename => MockFileWrapperBackingStore.ContainsKey(filename)));
+
+            return fileWrapper;
+        }
+
+        protected virtual IMicrosoftBuildProjectLoader BuildMockMicrosoftBuildProjectLoader()
+        {
+            var loader = MockRepository.GenerateStub<IMicrosoftBuildProjectLoader>();
+
+            //LoadMicrosoftBuildProject
+            loader.Stub(x => x.LoadMicrosoftBuildProject(Arg<string>.Is.Anything))
+                .Do(
+                    (Func<string, Project>)
+                        (filename =>
+                        {
+                            var loadedProjects = ProjectCollection.GlobalProjectCollection.GetLoadedProjects(filename);
+
+                            foreach(var loadedProject in loadedProjects)
+                                ProjectCollection.GlobalProjectCollection.UnloadProject(loadedProject);
+
+                            string result;
+                            if (!MockFileWrapperBackingStore.TryGetValue(filename, out result))
+                                throw new Exception(
+                                    string.Format(
+                                        "Failed to read [{0}].  Key is not present in MockFileReaderBackingStore",
+                                        filename));
+
+
+                            return new Project(new XmlTextReader(new StringReader(result)))
+                            {
+                                FullPath = filename
+                            };
+                        }));
+
+
+
+            return loader;
+        }
+    }
+}

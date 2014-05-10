3c187104a7b3d3e6631f6db0eb39c1e34b090da1
Philip Pittle <ppittlejunk@hotmail.com>
2014-05-11 01:59:52 +0200

    Added functionality for CodeGeneratorResponseWriter to add code behind to Project

---
+++ CopaceticSoftware.CodeGenerator.StarterKit/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="StringExtensions.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Sunday, May 11, 2014 1:49:49 AM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+namespace CopaceticSoftware.CodeGenerator.StarterKit.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string IfEmpty(this string s, string defaultValue)
+        {
+            return string.IsNullOrEmpty(s)
+                ? defaultValue
+                : s;
+        }
+    }
+}

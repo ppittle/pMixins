3dc463e44efa74f1754b3fc0ea56ac7ec5087315
Philip Pittle <ppittlejunk@hotmail.com>
2014-05-11 02:00:37 +0200

    New Tests

--- CopaceticSoftware.CodeGenerator.StarterKit.Tests/IntegrationTests/CodeGeneratorTests/OnItemSaveCodeGeneratorTests.cs
+++ CopaceticSoftware.CodeGenerator.StarterKit.Tests/IntegrationTests/CodeGeneratorTests/OnItemSaveCodeGeneratorTests.cs
@@ -16,12 +16,209 @@
 // </copyright> 
 //-----------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure;
+using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure.VisualStudioSolution;
 using CopaceticSoftware.pMixins.Tests.Common;
+using CopaceticSoftware.pMixins.VisualStudio.CodeGenerators;
+using Ninject;
+using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace CopaceticSoftware.CodeGenerator.StarterKit.Tests.IntegrationTests.CodeGeneratorTests
 {
-    [TestNotWrittenYet]
-    public class OnItemSaveCodeGeneratorTests : IntegrationTestBase
+    
+    public abstract class OnItemSaveCodeGeneratorTests : MockSolutionTestBase
     {
+        protected pMixinsOnItemSaveCodeGenerator _PMixinsOnItemSaveCodeGenerator;
+
+        private IVisualStudioCodeGenerator _mockVisualStudioCodeGenerator;
+
+        private VisualStudioCodeGenerator _actualVisualStudioCodeGenerator;
+
+        public override void MainSetup()
+        {
+            base.MainSetup();
+
+            _PMixinsOnItemSaveCodeGenerator = TestSpecificKernel.Get<pMixinsOnItemSaveCodeGenerator>();
+
+            SetupCodeGeneratorMock();
+
+            TestSpecificKernel.Rebind<IVisualStudioCodeGenerator>().ToMethod(x => _mockVisualStudioCodeGenerator);
+        }
+
+        private void SetupCodeGeneratorMock()
+        {
+            _actualVisualStudioCodeGenerator = TestSpecificKernel.Get<VisualStudioCodeGenerator>();
+
+            _mockVisualStudioCodeGenerator = MockRepository.GenerateMock<IVisualStudioCodeGenerator>();
+
+            _mockVisualStudioCodeGenerator.Stub(
+                x => x.GenerateCode(Arg<IEnumerable<ICodeGeneratorContext>>.Is.Anything))
+                    .Do(
+                        (Func<IEnumerable<ICodeGeneratorContext>, IEnumerable<CodeGeneratorResponse>>)
+                        (_actualVisualStudioCodeGenerator.GenerateCode)
+                    );
+
+            _mockVisualStudioCodeGenerator.Stub(
+                x => x.GenerateCode(Arg<IEnumerable<RawSourceFile>>.Is.Anything))
+                    .Do(
+                        (Func<IEnumerable<RawSourceFile>, IEnumerable<CodeGeneratorResponse>>)
+                        (_actualVisualStudioCodeGenerator.GenerateCode)
+                    );
+        }
+    }
+
+    public class OnSolutionOpening : OnItemSaveCodeGeneratorTests
+    {
+        private readonly MockSourceFile _mixinSourceFile = 
+            new MockSourceFile
+                {
+                    FileName = Path.Combine(MockSolution.MockSolutionFolder, "Mixin.cs"),
+                    Source = 
+                        @"namespace Testing{
+                            public class Mixin{ public void AMethod(){} }
+                        }"
+                };
+
+        private readonly MockSourceFile _mixinSourceFileSecondProject =
+            new MockSourceFile
+            {
+                FileName = Path.Combine(MockSolution.MockSolutionFolder, "Mixin2.cs"),
+                Source =
+                    @"namespace Testing{
+                            public class MixinOtherProject{ public void AMethod(){} }
+                        }"
+            };
+
+        private readonly MockSourceFile _targetSourceFile = 
+            new MockSourceFile
+                {
+                    FileName = Path.Combine(MockSolution.MockSolutionFolder, "Target.cs"),
+                    Source =
+                        @"namespace Testing{
+                            [CopaceticSoftware.pMixins.Attributes.pMixin(Mixin = typeof(Mixin))]
+                            [CopaceticSoftware.pMixins.Attributes.pMixin(Mixin = typeof(MixinOtherProject))]
+                            public partial class Target  {}
+                        }"
+                };
+
+        public override void MainSetup()
+        {
+            base.MainSetup();
+
+            _MockSolution.Projects.Add(new MockProject()
+                {
+                    FileName = Path.Combine(MockSolution.MockSolutionFolder, "OtherProject.csproj"),
+                    MockSourceFiles =
+                    {
+                        _mixinSourceFileSecondProject
+                    }
+                });
+
+            _MockSolution.Projects.Add(new MockProject()
+            {
+                MockSourceFiles =
+                {
+                    _mixinSourceFile,
+                    _targetSourceFile
+                },
+                ProjectReferences =
+                {
+                    _MockSolution.Projects[0]
+                }
+            });
+
+            //Simulate Solution Opening event
+            EventProxy.FireOnSolutionOpening(this, new EventArgs());
+        }
+
+        [Test]
+        public void TargetIsUpdatedWhenMixinInSameProjectIsSaved()
+        {
+            //Set Expectations
+            var generatedFile = _targetSourceFile.FileName.Replace(".cs", ".mixin.cs");
+
+            _MockMicrosoftBuildProjectLoader.Expect(
+                x => x.AddAutogeneratedClassFile(
+                    Arg<string>.Is.Equal(_MockSolution.Projects[1].FileName),
+                    Arg<string>.Is.Equal(generatedFile),
+                    Arg<string>.Is.Equal(_targetSourceFile)));
+
+            _MockFileWrapper.Expect(
+                x => x.WriteAllText(
+                    Arg<string>.Is.Equal(generatedFile),
+                    Arg<string>.Is.NotNull));
+
+            //Simulate Save on _mixinSourceFile
+            EventProxy.FireOnProjectItemSaved(this, 
+                new ProjectItemSavedEventArgs
+                {
+                    ClassFullPath = _mixinSourceFile.FileName,
+                    ProjectFullPath = _MockSolution.Projects[1].FileName
+                });
+
+            //Verify Expectations
+            _MockFileWrapper.VerifyAllExpectations();
+            _MockMicrosoftBuildProjectLoader.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void TargetIsUpdatedWhenMixinInReferencedProjectIsSaved()
+        {
+            //Set Expectations
+            var generatedFile = _targetSourceFile.FileName.Replace(".cs", ".mixin.cs");
+
+            _MockMicrosoftBuildProjectLoader.Expect(
+                x => x.AddAutogeneratedClassFile(
+                    Arg<string>.Is.Equal(_MockSolution.Projects[1].FileName),
+                    Arg<string>.Is.Equal(generatedFile),
+                    Arg<string>.Is.Equal(_targetSourceFile)));
+
+            _MockFileWrapper.Expect(
+                x => x.WriteAllText(
+                    Arg<string>.Is.Equal(generatedFile),
+                    Arg<string>.Is.NotNull));
+
+            //Simulate Save on _mixinSourceFileSecondProject
+            EventProxy.FireOnProjectItemSaved(this,
+                new ProjectItemSavedEventArgs
+                {
+                    ClassFullPath = _mixinSourceFileSecondProject.FileName,
+                    ProjectFullPath = _MockSolution.Projects[0].FileName
+                });
+
+            //Verify Expectations
+            _MockFileWrapper.VerifyAllExpectations();
+            _MockMicrosoftBuildProjectLoader.VerifyAllExpectations();
+        }
+    }
+
+    [TestNotWrittenYet]
+    public class OnItemSave : OnItemSaveCodeGeneratorTests
+    {
+        [Test]
+        public void CodeIsGenerated()
+        {
+            
+        }
+
+        [Test]
+        public void NewDependenciesAreDiscovered() { }
+
+        [Test]
+        public void HandlesCircularReference() { }
+    }
+
+    [TestNotWrittenYet]
+    public class OnProjectRemoved : OnItemSaveCodeGeneratorTests
+    {
+        [Test]
+        public void DependenciesAreUpdated()
+        {
+        }
+
     }
 }

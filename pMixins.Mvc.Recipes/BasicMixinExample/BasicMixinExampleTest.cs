e54d2595ee65d2fef9cbd1e1409924f3adcfd66f
philip pittle <ppittlejunk@hotmail.com>
2014-06-25 18:49:48 +0200

    Added example test

--- pMixins.Mvc.Recipes/BasicMixinExample/BasicMixinExampleTest.cs
+++ pMixins.Mvc.Recipes/BasicMixinExample/BasicMixinExampleTest.cs
@@ -1,7 +1,44 @@
+using NUnit.Framework;
+
 namespace pMixins.Mvc.Recipes.BasicMixinExample
 {
-    public class BasicMixinExampleTest
+    [TestFixture]
+    public class BasicMixinExampleTest 
     {
-        
+        [Test]
+        public void CanCallHelloWorld()
+        { 
+            Assert.AreEqual(
+                new BasicMixinExample().GetHelloWorld(),
+                "Hello World");
+        }
+
+        [Test]
+        public void CanGetAnswerToTheUniverse()
+        {
+            Assert.AreEqual(
+               new BasicMixinExample().AnswerToTheUniverse(),
+               42);
+        }
+
+        [Test]
+        public void CanDoImplicitConversion()
+        {
+            HelloWorldMixin helloWorld = new BasicMixinExample();
+
+            Assert.AreEqual(
+                helloWorld.GetHelloWorld(),
+                "Hello World");
+        }
+
+        [Test]
+        public void CanDoExplicitConversion()
+        {
+            var answerToTheUniverse = (AnswerToTheUniverseMixin) new BasicMixinExample();
+
+            Assert.AreEqual(
+               answerToTheUniverse.AnswerToTheUniverse(),
+               42);
+        }
     }
 }

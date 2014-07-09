08a842b8353cc435954d50b0d22754c2e3b8e32b
philip pittle <ppittlejunk@hotmail.com>
2014-07-09 17:14:51 +0200

    Added HowItWorks page

---
+++ pMixins.Mvc/Controllers/HowItWorksController.cs
@@ -0,0 +1,35 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="HowItWorksController.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Wednesday, July 9, 2014 5:14:59 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CopaceticSoftware.pMixins.Mvc.Controllers
+{
+    public class HowItWorksController : Controller
+    {
+        // GET: HowItWorks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

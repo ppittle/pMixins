c6b50eeedc1f40a40cb3477a7500400d4f3a6023
philip pittle <ppittlejunk@hotmail.com>
2014-07-09 17:06:16 +0200

    Introduction Copy

---
+++ pMixins.Mvc.Recipes/Introduction/AspectIntroduction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CopaceticSoftware.pMixins.Attributes;
+using CopaceticSoftware.pMixins.Interceptors;
+
+// ReSharper disable CheckNamespace
+namespace pMixins.Mvc.Recipes.Introduction.Aspect
+// ReSharper restore CheckNamespace
+{
+    public class Aspect : MixinInterceptorBase
+    {
+        public override void OnBeforeMethodInvocation(object sender, MethodEventArgs eventArgs)
+        {
+            Console.WriteLine("Before " + eventArgs.MemberName);
+        }
+
+        public override void OnAfterMethodInvocation(object sender, MethodEventArgs eventArgs)
+        {
+            Console.WriteLine("After " + eventArgs.MemberName);
+
+            eventArgs.ReturnValue = eventArgs.ReturnValue + " - Now with Aspects!";
+        }
+    }
+    
+    [pMixin(Mixin = typeof(HelloWorld), 
+        Interceptors = new[] { typeof(Aspect) })]
+    public partial class AspectIntroduction
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new AspectIntroduction().SayHello());
+        }
+    }
+
+
+}
---
+++ pMixins.Mvc.Recipes/Introduction/Introduction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CopaceticSoftware.pMixins.Attributes;
+using CopaceticSoftware.pMixins.Interceptors;
+
+namespace pMixins.Mvc.Recipes.Introduction
+{
+    public class HelloWorld
+    {
+        public string SayHello()
+        {
+            Console.WriteLine("Saying Hello");
+            return "Hello World!";
+        }
+    }
+
+    [pMixin(Mixin = typeof(HelloWorld))]
+    public partial class Introduction
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Introduction().SayHello());
+        }
+    }
+    
+}

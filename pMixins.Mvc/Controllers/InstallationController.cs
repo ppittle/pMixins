2a2031985f934f978acf552fe2e91a52f571c34d
philip pittle <ppittlejunk@hotmail.com>
2014-07-18 16:20:16 +0200

    Added Installation page

---
+++ pMixins.Mvc/Controllers/InstallationController.cs
@@ -0,0 +1,31 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="InstallationController.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Friday, July 18, 2014 2:03:59 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using System.Web.Mvc;
+
+namespace CopaceticSoftware.pMixins.Mvc.Controllers
+{
+    public class InstallationController : Controller
+    {
+        // GET: Installation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

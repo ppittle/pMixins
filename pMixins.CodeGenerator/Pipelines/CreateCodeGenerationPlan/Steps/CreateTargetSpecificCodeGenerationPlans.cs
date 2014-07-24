c482973ed17c318b0ad4805c112c079ac5a9ad41
philip pittle <ppittlejunk@hotmail.com>
2014-07-24 16:01:53 +0200

    Create Create Code Generator Plan step

---
+++ pMixins.CodeGenerator/Pipelines/CreateCodeGenerationPlan/Steps/CreateTargetSpecificCodeGenerationPlans.cs
@@ -0,0 +1,41 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="CreateTagetSpecificCodeGenerationPlans.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Thursday, July 24, 2014 3:54:20 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using CopaceticSoftware.Common.Patterns;
+using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure.CodeGenerationPlan;
+
+namespace CopaceticSoftware.pMixins.CodeGenerator.Pipelines.CreateCodeGenerationPlan.Steps
+{
+    public class CreateTargetSpecificCodeGenerationPlans : IPipelineStep<ICreateCodeGenerationPlanPipelineState>
+    {
+        public bool PerformTask(ICreateCodeGenerationPlanPipelineState manager)
+        {
+            foreach (var target in manager.CommonState.SourcePartialClassDefinitions)
+            {
+                manager.CodeGenerationPlans.Add(
+                    target, 
+                    new CodeGenerationPlan
+                    {
+                        SourceClass = target
+                    });
+            }
+
+            return true;
+        }
+    }
+}

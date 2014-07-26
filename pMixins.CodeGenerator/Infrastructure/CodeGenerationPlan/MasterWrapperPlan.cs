76811e4e767da65f8c71101b977495d7a99e5a44
philip pittle <ppittlejunk@hotmail.com>
2014-07-26 12:22:13 +0200

    Separated Plan classes to dedicated files

---
+++ pMixins.CodeGenerator/Infrastructure/CodeGenerationPlan/MasterWrapperPlan.cs
@@ -0,0 +1,41 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="MasterWrapperPlan.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Saturday, July 26, 2014 12:21:54 PM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace CopaceticSoftware.pMixins.CodeGenerator.Infrastructure.CodeGenerationPlan
+{
+    public class MasterWrapperPlan
+    {
+        /// <summary>
+        /// The variable name for the data member that holds a reference
+        /// to the Mixin (with a type of IRequirements interface).
+        /// </summary>
+        public const string MixinInstanceDataMemberName = "_mixinInstance";
+
+        public IEnumerable<MemberWrapper> StaticMembers { get; set; }
+
+        public IEnumerable<MemberWrapper> ProtectedAbstractMembers { get; set; }
+
+        public IEnumerable<MemberWrapper> VirtualMembers { get; set; }
+
+        public IEnumerable<MemberWrapper> RegularMembers { get; set; }
+
+        public string MasterWrapperClassName { get; set; }
+    }
+}

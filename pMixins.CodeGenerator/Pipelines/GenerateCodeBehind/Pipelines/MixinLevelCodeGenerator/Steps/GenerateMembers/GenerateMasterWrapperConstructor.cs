04e37722c95aba87d627ea6dac532eaae49ef550
philip pittle <ppittlejunk@hotmail.com>
2014-07-30 11:35:58 +0200

    Split up Master Wrapper Constructor and Data Members pipeline steps and added Code Generation for Master Wrapper Virtual Member Function data members

--- pMixins.CodeGenerator/Pipelines/GenerateCodeBehind/Pipelines/MixinLevelCodeGenerator/Steps/GenerateMembers/GenerateMasterWrapperConstructor.cs
+++ pMixins.CodeGenerator/Pipelines/GenerateCodeBehind/Pipelines/MixinLevelCodeGenerator/Steps/GenerateMembers/GenerateMasterWrapperConstructor.cs
@@ -1,9 +1,28 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="GenerateMasterWrapperConstructor.cs" company="Copacetic Software"> 
+// Copyright (c) Copacetic Software.  
+// <author>Philip Pittle</author> 
+// <date>Wednesday, July 30, 2014 10:23:53 AM</date> 
+// Licensed under the Apache License, Version 2.0,
+// you may not use this file except in compliance with this License.
+//  
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright> 
+//-----------------------------------------------------------------------
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CopaceticSoftware.CodeGenerator.StarterKit.Extensions;
 using CopaceticSoftware.Common.Extensions;
 using CopaceticSoftware.Common.Patterns;
+using CopaceticSoftware.pMixins.CodeGenerator.Extensions;
 using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure;
 using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure.CodeGenerationPlan;
 using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure.CodeGeneratorProxy;

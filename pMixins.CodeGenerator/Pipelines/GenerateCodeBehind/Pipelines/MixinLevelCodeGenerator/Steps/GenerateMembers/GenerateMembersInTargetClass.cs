96b4ba9db91f259dd996051e32329ed5aa2b61f4
philip pittle <ppittlejunk@hotmail.com>
2014-07-30 18:06:35 +0200

    Removed using statement

--- pMixins.CodeGenerator/Pipelines/GenerateCodeBehind/Pipelines/MixinLevelCodeGenerator/Steps/GenerateMembers/GenerateMembersInTargetClass.cs
+++ pMixins.CodeGenerator/Pipelines/GenerateCodeBehind/Pipelines/MixinLevelCodeGenerator/Steps/GenerateMembers/GenerateMembersInTargetClass.cs
@@ -19,7 +19,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using CopaceticSoftware.CodeGenerator.StarterKit.Extensions;
-using CopaceticSoftware.Common.Extensions;
 using CopaceticSoftware.Common.Patterns;
 using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure;
 using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure.CodeGenerationPlan;

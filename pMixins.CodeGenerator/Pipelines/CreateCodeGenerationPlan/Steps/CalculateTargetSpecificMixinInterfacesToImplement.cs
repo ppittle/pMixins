3e2ad95a1abebb446a994e9b2aee19ce48168d33
philip pittle <ppittlejunk@hotmail.com>
2014-07-30 19:16:33 +0200

    Migrated in code to ensure MixinDependencies are satisifed

--- pMixins.CodeGenerator/Pipelines/CreateCodeGenerationPlan/Steps/CalculateTargetSpecificMixinInterfacesToImplement.cs
+++ pMixins.CodeGenerator/Pipelines/CreateCodeGenerationPlan/Steps/CalculateTargetSpecificMixinInterfacesToImplement.cs
@@ -18,9 +18,12 @@
 
 using System.Linq;
 using CopaceticSoftware.CodeGenerator.StarterKit.Extensions;
+using CopaceticSoftware.CodeGenerator.StarterKit.Infrastructure;
 using CopaceticSoftware.Common.Patterns;
 using CopaceticSoftware.pMixins.Attributes;
 using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure.CodeGenerationPlan;
+using CopaceticSoftware.pMixins.CodeGenerator.Pipelines.ResolveAttributes.Infrastructure;
+using CopaceticSoftware.pMixins.Infrastructure;
 using ICSharpCode.NRefactory.TypeSystem;
 
 namespace CopaceticSoftware.pMixins.CodeGenerator.Pipelines.CreateCodeGenerationPlan.Steps
@@ -44,7 +47,103 @@ namespace CopaceticSoftware.pMixins.CodeGenerator.Pipelines.CreateCodeGeneration
                         .Where(bt => bt.Kind == TypeKind.Interface)
                         //Filter out interfaces decorated with DoNotMixin
                         .Where(bt => !bt.IsDecoratedWithAttribute(doNotMixinIType))
-                        .Distinct();
+                        .Distinct()
+                        .ToList();
+
+                if (!EnsureMixinDependenciesAreSatisfied(manager, cgp))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static readonly string _mixinDependencyTypeName = typeof(IMixinDependency<>).GetOriginalFullName()
+            //This is hacky, but can't find a way to compare typeof(IMixinDependency<>) to new IType(IMixinDependency<int>)
+            .Replace("<>", "");
+
+        public static bool TypeIsIMixinDependency(IType type)
+        {
+            return type.Kind == TypeKind.Interface &&
+                   type.GetOriginalFullName().StartsWith(_mixinDependencyTypeName);
+        }
+
+        private bool EnsureMixinDependenciesAreSatisfied(
+            ICreateCodeGenerationPlanPipelineState manager,
+            CodeGenerationPlan cgp)
+        {
+            var mixinBaseTypeMap =
+                cgp.MixinGenerationPlans.Values
+                    .Select(mgp => mgp.MixinAttribute)
+                    .ToDictionary(
+                        x => x.Mixin.GetOriginalFullName(),
+                        x => x.Mixin.GetAllBaseTypes());
+
+            var allBaseTypes = 
+                mixinBaseTypeMap
+                    .SelectMany(x => x.Value)
+                    .Union(
+                        cgp.SourceClass.BaseTypes
+                            .Select(bt => manager.CommonState.Context.TypeResolver.Resolve(bt).Type))
+                    .ToList();
+
+          var unsatisfiedDependencies =
+                mixinBaseTypeMap
+                    //Take only Mixin base types
+                    .SelectMany(x => x.Value)
+                    //Get IMixinDependency
+                    .Where(bt => TypeIsIMixinDependency(bt))
+                    //Cast in order to get generic parameter
+                    .OfType<ParameterizedType>()
+                    //Pull out generic param
+                    .Select(bt => bt.TypeArguments.First())
+                    //Filter out 
+                    .Where(md => !allBaseTypes.Contains(md))
+                    .ToList();
+
+            //Add interface dependencies to Target
+            cgp.TargetCodeBehindPlan.MixinInterfaces.AddRange(
+                unsatisfiedDependencies
+                    .Where(md => md.Kind == TypeKind.Interface));
+
+            var unsatisfiedClassDependencies =
+                unsatisfiedDependencies
+                    .Where(md => md.Kind != TypeKind.Interface)
+                    .ToList();
+
+            //Write out errors
+            if (unsatisfiedClassDependencies.Count > 0)
+            {
+                foreach (var dep in unsatisfiedDependencies)
+                {
+                    var mixin =
+                       cgp.MixinGenerationPlans.Values.Select(mgp => mgp.MixinAttribute)
+                            .First(mix =>
+                                mix.Mixin.FullName ==
+                                mixinBaseTypeMap
+                                    .First(x =>
+                                        x.Value.OfType<ParameterizedType>()
+                                            .Any(p => p.TypeArguments.First().Equals(dep)))
+                                    .Key);
+
+            
+                    manager.CommonState.CodeGenerationErrors.Add(
+                        new CodeGenerationError
+                        {
+                            
+                            Message = string.Format(
+                                Strings.ErrorMixinDependencyIsClassAndIsNotSatisified,
+                                mixin.Mixin.GetOriginalFullName(),
+                                cgp.SourceClass.Name,
+                                dep.GetOriginalFullName()),
+
+
+                            Line = (uint)cgp.SourceClass.GetRegion().BeginLine,
+                            Column = (uint)cgp.SourceClass.GetRegion().EndLine,
+                            Severity = CodeGenerationError.SeverityOptions.Error
+                        });
+                }
+
+                return false;
             }
 
             return true;

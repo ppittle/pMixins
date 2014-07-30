03484ef2ad29e50571109fa57747da6dfd6375df
philip pittle <ppittlejunk@hotmail.com>
2014-07-30 23:20:53 +0200

    Removed using statement

--- pMixins.CodeGenerator/Pipelines/GenerateCodeBehind/Pipelines/TargetLevelCodeGenerator/Steps/CreateTypeDeclarations/CreateSharedRequirementsInterfaceTypeDeclaration.cs
+++ pMixins.CodeGenerator/Pipelines/GenerateCodeBehind/Pipelines/TargetLevelCodeGenerator/Steps/CreateTypeDeclarations/CreateSharedRequirementsInterfaceTypeDeclaration.cs
@@ -18,7 +18,6 @@
 
 using CopaceticSoftware.CodeGenerator.StarterKit.Extensions;
 using CopaceticSoftware.Common.Patterns;
-using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure;
 using CopaceticSoftware.pMixins.CodeGenerator.Infrastructure.CodeGeneratorProxy;
 using CopaceticSoftware.pMixins.CodeGenerator.Pipelines.GenerateCodeBehind.Pipelines.TargetLevelCodeGenerator.Steps.GenerateMembers;
 using ICSharpCode.NRefactory.CSharp;

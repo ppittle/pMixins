0dc8f27662f11b20a729c03596b71c2eb2ae9cae
philip pittle <ppittlejunk@hotmail.com>
2014-08-20 15:00:46 +0200

    Moved ResolveInterceptorMixinRequirements to separate step

--- pMixins.CodeGenerator/Pipelines/ResolveAttributes/Steps/ResolveAttributes.cs
+++ pMixins.CodeGenerator/Pipelines/ResolveAttributes/Steps/ResolveAttributes.cs
@@ -17,13 +17,8 @@
 //-----------------------------------------------------------------------
 
 using System.Collections.Generic;
-using System.Linq;
-using CopaceticSoftware.CodeGenerator.StarterKit.Extensions;
 using CopaceticSoftware.Common.Patterns;
-using CopaceticSoftware.pMixins.Attributes;
-using CopaceticSoftware.pMixins.CodeGenerator.Pipelines.ResolveAttributes.Infrastructure;
 using CopaceticSoftware.pMixins.CodeGenerator.Pipelines.ResolveAttributes.Steps.AttributeSpecificPipelines.pMixin;
-using ICSharpCode.NRefactory.TypeSystem;
 
 namespace CopaceticSoftware.pMixins.CodeGenerator.Pipelines.ResolveAttributes.Steps
 {
@@ -41,66 +36,6 @@ namespace CopaceticSoftware.pMixins.CodeGenerator.Pipelines.ResolveAttributes.St
             return (_attributePipelines.RunPipeline(pipelineState,
                                                     haltOnStepFailing: null,
                                                     throwException: null));
-        }
-    }
-
-    public class ResolveInterceptorMixinRequirements : IPipelineStep<IResolveAttributesPipelineState>
-    {
-        public bool PerformTask(IResolveAttributesPipelineState manager)
-        {
-            foreach (var classAtts in manager.CommonState.SourcePartialClassAttributes)
-            {
-                var allMixinAttributes = manager.PartialClassLevelResolvedPMixinAttributes[classAtts.Key]
-                                        .OfType<pMixinAttributeResolvedResult>()
-                                        .ToList();
-
-
-                var interceptors = allMixinAttributes.SelectMany(x => x.Interceptors);
-
-                var interceptorRequiredAttributes =
-                    interceptors
-                        .SelectMany(x => x.GetAttributes())
-                        .Where(a => a.AttributeType.Implements<InterceptorMixinRequirementAttribute>());
-
-                var mixinsToAdd =
-                    interceptorRequiredAttributes
-                        .Select(x => ResolveMixinType(x, manager))
-                        .Where(x => x != null)
-                        .Distinct()
-                        .Where(x => !allMixinAttributes.Select(a => a.Mixin).Contains(x));
-
-
-                foreach (var mixin in mixinsToAdd)
-                {
-                     manager.PartialClassLevelResolvedPMixinAttributes[classAtts.Key]
-                        .Add(new pMixinAttributeResolvedResult(null){Mixin = mixin});
-                }
-            }
-
-            return true;
-        }
-
-        private IType ResolveMixinType(IAttribute attribute, IResolveAttributesPipelineState manager)
-        {
-            IType result = null;
-
-            manager.TypeInstanceActivator.TryCreateInstance<pMixinAttributeResolvedResult>(
-                attribute,
-                instance =>
-                {
-                    try
-                    {
-                        result = instance.Mixin;
-                    }
-                    catch
-                    {
-                    }
-                });
-
-            if (null != result)
-                return result;
-
-            return attribute.GetNamedArgumentValue("Mixin") as IType;
         }
     }
 }
